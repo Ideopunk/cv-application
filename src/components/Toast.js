98352e3758049e76a8685268f2f33d5faed1d49c
Conor Barnes <conorbarnes93@gmail.com>
2020-09-29 12:35:25 -0700

    Fix lingering toast / id issue, use classnames

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class Toast extends Component {
 	componentDidMount = () => {
-        let toast = document.getElementById("toast")
-        setTimeout(() => toast.classList.add("transition"), 0);
-		setTimeout(() => toast.classList.add("hide"), 1000);
+        let toasts = document.getElementsByClassName("toast")
+        for (let toast of toasts) {
+            setTimeout(() => toast.classList.add("transition"), 0);
+            setTimeout(() => toast.classList.add("hide"), 1000);
+        }
 	};
 
 	render() {
