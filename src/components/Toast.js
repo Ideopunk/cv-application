40ceec9263a11e8dd9730e47e973b6415cd407c4
Conor Barnes <conorbarnes93@gmail.com>
2020-09-29 17:45:31 -0700

    Convert Toast component to function

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
-class Toast extends Component {
-	componentDidMount = () => {
-        let toasts = document.getElementsByClassName("toast")
-        for (let toast of toasts) {
-            setTimeout(() => toast.classList.add("transition"), 0);
-            setTimeout(() => toast.classList.add("hide"), 1000);
-        }
-	};
+function Toast() {
+	useEffect(() => {
+		let toasts = document.getElementsByClassName("toast");
+		for (let toast of toasts) {
+			setTimeout(() => toast.classList.add("transition"), 0);
+			setTimeout(() => toast.classList.add("hide"), 1000);
+		}
+	});
 
-	render() {
-		return (
-			<div id="toast" className="toast">
-				Thank you for your submission! We've updated your profile!
-			</div>
-		);
-	}
+	return (
+		<div id="toast" className="toast">
+			Thank you for your submission! We've updated your profile!
+		</div>
+	);
 }
 
 export default Toast;
